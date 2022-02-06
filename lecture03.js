10c03d36c87e4028afc32089f47b273565c8f6c3
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-02-06 19:50:23 +0900

    변수 활용

--- lecture03.js
+++ lecture03.js
@@ -1,4 +1,7 @@
-var msg;
-msg = "Hello World";
-// alert(msg);
-console.log(msg);
+// var msg;
+// msg = "Hello World";
+// // alert(msg);  //새로고침 할때 나타냄
+// console.log(msg);  // 콘솔에 나타냄
+
+var name=prompt("이름을 입력해 주세요.");
+console.log(name, "님 환영합니다.");

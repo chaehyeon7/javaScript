c9612f4b1631bfd04e202872e0900e2b7d671714
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-02-10 15:44:02 +0900

    do_while문

---
+++ while/do_while.js
@@ -0,0 +1,14 @@
+var cond = false;
+
+while(cond){
+    console.log("이 구문은 실행되지 않음");
+}
+
+do{
+    console.log("이 구문은 한번은 실행됨");
+}while(cond);
+do{
+    var ans = parseInt(prompt("1+1=?"));
+}while(ans != 2);
+
+console.log("맞췄습니다.");
